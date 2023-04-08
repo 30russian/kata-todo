c7b1df819fbf2db63abcd9b3673492b6d899b599
30russian <30russian@gmail.com>
2023-04-08 15:17:44 +0300

    Refactor NewTaskForm component for using hooks

--- src/components/NewTaskForm/NewTaskForm.jsx
+++ src/components/NewTaskForm/NewTaskForm.jsx
@@ -1,88 +1,92 @@
 import './NewTaskForm.scss';
 
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 
-export default class NewTaskForm extends Component {
-  state = {
-    description: '',
-    minutes: '',
-    seconds: '',
-  };
-
-  static propTypes = {
-    onNewTask: PropTypes.func.isRequired,
-  };
-
-  onDescriptionChange = (event) => {
-    this.setState({ description: event.target.value });
-  };
-
-  onTimerChange = (event) => {
+const timePartChanger = (changer) => {
+  return (event) => {
     event.preventDefault();
-    const { name, value } = event.target;
+    const value = event.target.value;
     if ((value.match(/\d{1,2}/g) && Number(value) > 0 && Number(value) < 60) || value === '') {
-      this.setState({ [name]: value });
+      changer(value);
     } else {
-      this.setState((state) => ({ [name]: state[name] }));
+      changer((curValue) => curValue);
     }
   };
+};
 
-  onNewTaskSubmit = (event) => {
-    const { minutes, seconds } = this.state;
-    const description = this.state.description.trim();
-    if (description.length > 0) {
-      this.props.onNewTask(description, Number(minutes), Number(seconds));
-      this.setState({ description: '', minutes: '', seconds: '' });
+const NewTaskForm = ({ onNewTask }) => {
+  const [description, setDescription] = useState('');
+  const [minutes, setMinutes] = useState('');
+  const [seconds, setSeconds] = useState('');
+
+  const onDescriptionChange = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const minutesChanger = timePartChanger(setMinutes);
+  const secondsChanger = timePartChanger(setSeconds);
+
+  const onNewTaskSubmit = (event) => {
+    const trimmedDescription = description.trim();
+    if (trimmedDescription.length > 0) {
+      onNewTask(trimmedDescription, Number(minutes), Number(seconds));
+      setDescription('');
+      setMinutes('');
+      setSeconds('');
     }
 
     event.preventDefault();
   };
 
-  render() {
-    return (
-      <header className='new-task-form'>
-        <h1>todos</h1>
-        <form onSubmit={this.onNewTaskSubmit} autoComplete='off'>
-          <input
-            id='new-task-form-id'
-            className='new-task-form__description'
-            onChange={this.onDescriptionChange}
-            placeholder='Task'
-            value={this.state.description}
-            required
-            autoFocus
-          />
-          <label htmlFor='new-task-form-id' className='visually-hidden'>
-            Field containing task description
-          </label>
-          <input
-            name='minutes'
-            id='new-task-form-minutes-id'
-            className='new-task-form__timer'
-            onChange={this.onTimerChange}
-            placeholder='Min'
-            value={this.state.minutes}
-          />
-          <label htmlFor='new-task-form-minutes-id' className='visually-hidden'>
-            Minutes
-          </label>
-          <input
-            name='seconds'
-            id='new-task-form-seconds-id'
-            className='new-task-form__timer'
-            onChange={this.onTimerChange}
-            placeholder='Sec'
-            value={this.state.seconds}
-          />
-          <label htmlFor='new-task-form-seconds-id' className='visually-hidden'>
-            Seconds
-          </label>
-          <button type='submit' hidden>
-            Submit
-          </button>
-        </form>
-      </header>
-    );
-  }
-}
+  return (
+    <header className='new-task-form'>
+      <h1>todos</h1>
+      <form onSubmit={onNewTaskSubmit} autoComplete='off'>
+        <input
+          id='new-task-form-id'
+          className='new-task-form__description'
+          onChange={onDescriptionChange}
+          placeholder='Task'
+          value={description}
+          required
+          autoFocus
+        />
+        <label htmlFor='new-task-form-id' className='visually-hidden'>
+          Field containing task description
+        </label>
+        <input
+          name='minutes'
+          id='new-task-form-minutes-id'
+          className='new-task-form__timer'
+          onChange={minutesChanger}
+          placeholder='Min'
+          value={minutes}
+        />
+        <label htmlFor='new-task-form-minutes-id' className='visually-hidden'>
+          Minutes
+        </label>
+        <input
+          name='seconds'
+          id='new-task-form-seconds-id'
+          className='new-task-form__timer'
+          onChange={secondsChanger}
+          placeholder='Sec'
+          value={seconds}
+        />
+        <label htmlFor='new-task-form-seconds-id' className='visually-hidden'>
+          Seconds
+        </label>
+        <button type='submit' hidden>
+          Submit
+        </button>
+      </form>
+    </header>
+  );
+};
+
+NewTaskForm.propTypes = {
+  onNewTask: PropTypes.func.isRequired,
+};
+
+export default NewTaskForm;
