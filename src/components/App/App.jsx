e60e4fd59d85f1c3087f8c48541e5dfe123e3204
30russian <30russian@gmail.com>
2023-04-09 15:26:28 +0300

    Refactor App component for using hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { parseISO } from 'date-fns';
-import { Component } from 'react';
+import { useEffect, useState } from 'react';
 
 import TaskList from '../TaskList';
 import Footer from '../Footer';
@@ -7,57 +7,45 @@ import NewTaskForm from '../NewTaskForm';
 
 import './App.scss';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  let lastId = 100;
 
-    this.lastId = 100;
-  }
+  const [tasks, setTasks] = useState([]);
+  const [filter, setFilter] = useState('all');
 
-  fillTasksFromLS() {
+  const fillTasksFromLS = () => {
     const tasks = JSON.parse(window.localStorage.getItem('tasks') ?? '[]');
     tasks.forEach((task) => {
       task.created = parseISO(task.created);
     });
-    this.setState({ tasks });
+    setTasks(tasks);
 
     if (tasks.length > 0) {
-      this.lastId = Math.max.apply(
+      lastId = Math.max.apply(
         null,
         tasks.map((task) => task.id)
       );
     }
-  }
+  };
 
-  componentDidMount() {
-    this.fillTasksFromLS();
+  useEffect(() => {
+    fillTasksFromLS();
 
     window.addEventListener('storage', () => {
-      this.fillTasksFromLS();
+      fillTasksFromLS();
     });
-  }
+  }, []);
 
-  state = {
-    tasks: [],
-    filter: 'all',
-  };
-
-  x = 0;
-
-  onComplete = (id) => {
-    this.setState((state) => {
-      const { tasks } = state;
+  const onComplete = (id) => {
+    setTasks((tasks) => {
       const idx = tasks.findIndex((el) => el.id === id);
       const newTask = { ...tasks[idx], completed: !tasks[idx].completed };
-      const newTasks = [...tasks.slice(0, idx), newTask, ...tasks.slice(idx + 1)];
-
-      return { tasks: newTasks };
+      return [...tasks.slice(0, idx), newTask, ...tasks.slice(idx + 1)];
     });
   };
 
-  onEdit = (id) => {
-    this.setState((state) => {
-      const { tasks } = state;
+  const onEdit = (id) => {
+    setTasks((tasks) => {
       const newTasks = [...tasks];
 
       const editingIdx = newTasks.findIndex((el) => el.editing);
@@ -70,26 +58,24 @@ export default class App extends Component {
       const newTask = { ...newTasks[idx], editing: true };
       newTasks.splice(idx, 1, newTask);
 
-      return { tasks: newTasks };
+      return newTasks;
     });
   };
 
-  onEditFinished = (description) => {
-    this.setState((state) => {
-      const { tasks } = state;
+  const onEditFinished = (description) => {
+    setTasks((tasks) => {
       const idx = tasks.findIndex((el) => el.editing);
       const newTask = { ...tasks[idx], description, editing: false };
       const newTasks = [...tasks.slice(0, idx), newTask, ...tasks.slice(idx + 1)];
 
       window.localStorage.setItem('tasks', JSON.stringify(newTasks));
 
-      return { tasks: newTasks };
+      return newTasks;
     });
   };
 
-  onTimerTicked = (id) => {
-    this.setState((state) => {
-      const { tasks } = state;
+  const onTimerTicked = (id) => {
+    setTasks((tasks) => {
       const idx = tasks.findIndex((el) => el.id === id);
       const newTask = { ...tasks[idx] };
       newTask.duration++;
@@ -97,27 +83,27 @@ export default class App extends Component {
 
       window.localStorage.setItem('tasks', JSON.stringify(newTasks));
 
-      return { tasks: newTasks };
+      return newTasks;
     });
   };
 
-  onDelete = (id) => {
-    this.setState((state) => {
-      const newTasks = [...state.tasks];
+  const onDelete = (id) => {
+    setTasks((tasks) => {
+      const newTasks = [...tasks];
       const idx = newTasks.findIndex((el) => el.id === id);
       newTasks.splice(idx, 1);
 
       window.localStorage.setItem('tasks', JSON.stringify(newTasks));
 
-      return { tasks: newTasks };
+      return newTasks;
     });
   };
 
-  onNewTask = (description, minutes, seconds) => {
-    this.setState((state) => {
-      const newTasks = [...state.tasks];
+  const onNewTask = (description, minutes, seconds) => {
+    setTasks((tasks) => {
+      const newTasks = [...tasks];
       newTasks.push({
-        id: ++this.lastId,
+        id: ++lastId,
         description,
         completed: false,
         created: new Date(),
@@ -126,39 +112,35 @@ export default class App extends Component {
 
       window.localStorage.setItem('tasks', JSON.stringify(newTasks));
 
-      return { tasks: newTasks };
+      return newTasks;
     });
   };
 
-  onFilterClicked = (filter) => {
-    this.setState({ filter });
+  const onFilterClicked = (filter) => {
+    setFilter(filter);
   };
 
-  onClearCompleted = () => {
-    this.setState((state) => ({ tasks: [...state.tasks].filter((item) => !item.completed) }));
+  const onClearCompleted = () => {
+    setTasks((tasks) => [...tasks].filter((item) => !item.completed));
   };
 
-  render() {
-    return (
-      <section className='app'>
-        <NewTaskForm onNewTask={this.onNewTask} />
-        <section className='app__main-section'>
-          <TaskList
-            tasks={this.state.tasks}
-            filter={this.state.filter}
-            onDelete={this.onDelete}
-            onComplete={this.onComplete}
-            onEdit={this.onEdit}
-            onEditFinished={this.onEditFinished}
-            onTimerTicked={this.onTimerTicked}
-          />
-          <Footer
-            tasks={this.state.tasks}
-            onFilterClicked={this.onFilterClicked}
-            onClearCompleted={this.onClearCompleted}
-          />
-        </section>
+  return (
+    <section className='app'>
+      <NewTaskForm onNewTask={onNewTask} />
+      <section className='app__main-section'>
+        <TaskList
+          tasks={tasks}
+          filter={filter}
+          onDelete={onDelete}
+          onComplete={onComplete}
+          onEdit={onEdit}
+          onEditFinished={onEditFinished}
+          onTimerTicked={onTimerTicked}
+        />
+        <Footer tasks={tasks} onFilterClicked={onFilterClicked} onClearCompleted={onClearCompleted} />
       </section>
-    );
-  }
-}
+    </section>
+  );
+};
+
+export default App;
